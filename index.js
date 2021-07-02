20846985379c7cae3a1ef2d5c14ac8945e44e5d6
小宇 <59905001+hu121711082@users.noreply.github.com>
2021-07-02 14:35:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 let btn_Yes = document.querySelector('.btn-Yes')
-        let btn_No = document.querySelector('.btn-No')
-        let num = 0
-        let p = document.querySelectorAll('.contenr p')
-        let btnBox = document.querySelector('.btn-box')
-        let noBox = document.querySelector('.no-box')
-        let yesBox = document.querySelector('.yes-box')
-        function yes() {
-            yesBox.style.display = 'block'
-            btnBox.style.display = 'none'
-            setTimeout(() => {
-                location.href('http://w1.zxgnz.cn/article/h5/20200517/15a5257d2d163fc62e0cb9cedf2d3fb8.html')
-            }, 2000);
-        }
-        function no() {
-            num++
-            // console.log(num)
-            if(num > 6) {
-                // console.log(btnBox)
-                btnBox.style.display = 'none'
-                noBox.style.display = 'block'
-            }else {
-                // console.log(p[num])
-                p[num].style.display = 'block'
-            }
-            
-        }
-        
+let btn_No = document.querySelector('.btn-No')
+let num = 0
+let p = document.querySelectorAll('.contenr p')
+let btnBox = document.querySelector('.btn-box')
+let noBox = document.querySelector('.no-box')
+let yesBox = document.querySelector('.yes-box')
 
-        function aa() {
-            btn_Yes.onclick = yes
-            btn_No.onclick = no
-        }
-        aa()
-        noBox.onclick = function() {
-            btnBox.style.display = 'block'
-            noBox.style.display = 'none'
-        }
+
+let wrapper = document.querySelector('.wrapper')
+let info = document.querySelector('.info')
+function yes() {
+    yesBox.style.display = 'block'
+    btnBox.style.display = 'none'
+    setTimeout(() => {
+        wrapper.style.display = 'none'
+        info.style.display = 'block'
+    }, 2000);
+}
+function no() {
+    num++
+    // console.log(num)
+    if(num > 6) {
+        // console.log(btnBox)
+        btnBox.style.display = 'none'
+        noBox.style.display = 'block'
+    }else {
+        // console.log(p[num])
+        p[num].style.display = 'block'
+    }
+    
+}
+
+
+function aa() {
+    btn_Yes.onclick = yes
+    btn_No.onclick = no
+}
+aa()
+noBox.onclick = function() {
+    btnBox.style.display = 'block'
+    noBox.style.display = 'none'
+}
